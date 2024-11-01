803742aef55fc11e1394c18abbc48bbf87bdd740
Akash Kennedy <akashkennedy1@gmail.com>
2024-11-01 06:52:44 +0530

    Create Title Cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,33 @@
 import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+import { useEffect, useRef } from "react";
 
 const TitleCards = () => {
-  return <div className="titlecards"></div>;
+  const cardsRef = useRef();
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
